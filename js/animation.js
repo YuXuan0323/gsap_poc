9b6a4f6181b3d60dca980daeafe154bb2b30f42c
Yushiuan <vivian32364@gmail.com>
2025-06-20 13:52:27 +0800

    update

--- js/animation.js
+++ js/animation.js
@@ -22,34 +22,103 @@ gsap.to( rocket,{
     }
 });
 
+// const stars = document.querySelectorAll('.star');
 const headerPic = document.querySelector('.header_pic');
 
-const stars = [
+// stars = [
+//   { top: '10%', left: '20%', rotate: 15 },
+//   { top: '30%', left: '40%', rotate: 45 },
+//   { top: '60%', left: '10%', rotate: -20 },
+//   { top: '80%', left: '70%', rotate: 60 },
+// ];
+
+// stars.forEach(function(star, index) {
+//   const img = document.createElement('img');
+//   img.src = './img/star.svg';
+//   img.className = 'star';
+//   img.style.top = star.top;
+//   img.style.left = star.left;
+//   img.style.transform = 'rotate(' + star.rotate + 'deg)';//星星沒有rotate
+//   img.style.height = '40px';
+//   img.style.position = 'absolute';
+//   headerPic.appendChild(img);
+// });
+
+// gsap.to( stars, {
+//   y: -200,   // why沒有往上移 
+//   ease: 'ease-in-out',
+//   scrollTrigger: {
+//     trigger: '.animation',
+//     start: 'top bottom',
+//     end: 'bottom top',
+//     onUpdate: () => {
+//     console.log("✨ 動畫更新中");
+//     }
+//     // scrub: true
+//   }
+// });
+
+// ⭐ 這是位置資料 → 命名改為 starPositions，避免覆蓋掉 DOM 變數
+const starPositions = [
   { top: '10%', left: '20%', rotate: 15 },
   { top: '30%', left: '40%', rotate: 45 },
   { top: '60%', left: '10%', rotate: -20 },
   { top: '80%', left: '70%', rotate: 60 },
 ];
 
-stars.forEach(function(star, index) {
+// ⭐ 動態產生星星
+starPositions.forEach((star) => {
   const img = document.createElement('img');
   img.src = './img/star.svg';
   img.className = 'star';
   img.style.top = star.top;
   img.style.left = star.left;
-  img.style.transform = 'rotate(' + star.rotate + 'deg)';//星星沒有旋轉
+  img.style.transform = `rotate(${star.rotate}deg)`;
   img.style.height = '40px';
   img.style.position = 'absolute';
   headerPic.appendChild(img);
 });
 
-gsap.to( stars, {
-  y: -200,   // 沒有往上移
-  ease: 'ease-in-out',
+const stars = document.querySelectorAll('.star');
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(stars, {
+  y: -200,
+  ease: 'easeInOut',
   scrollTrigger: {
     trigger: '.animation',
     start: 'top bottom',
     end: 'bottom top',
+    scrub: true,
+    markers: true,
+    onUpdate: () => {
+      console.log("✨ 動畫更新中");
+    }
+  }
+});
+
+
+gsap.to('.cloud', {
+  y: -500,
+  ease: 'sine.inOut',
+  scrollTrigger: {
+    trigger: '.haeder-layout',
+    start: 'top top', 
+    end: 'bottom top', 
+    scrub: true
+  }
+});
+
+gsap.to('.animation', {
+  y: -250,
+  opacity: 1,
+  delay: 1,
+  ease: 'sine.inOut',
+  scrollTrigger: {
+    trigger: '.haeder-layout',
+    start: 'top top', 
+    end: 'bottom top', 
     scrub: true
   }
 });
